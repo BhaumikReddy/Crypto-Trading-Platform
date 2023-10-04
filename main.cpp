2ddc6b24d6ff902260cb3599dc81e746245da43f
BhaumikReddy <saibhaumikreddy@gmail.com>
2023-10-04 12:17:45 +0530

    Menu was made

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"1: Print Help "<<std::endl;
+
+    std::cout<<"2: Print Exchange Stats "<<std::endl;
+    
+    std::cout<<"3: Make an Offer "<<std::endl;
+
+    std::cout<<"4: Make a Bid "<<std::endl;
+
+    std::cout<<"5: Print Wallet "<<std::endl;
+
+    std::cout<<"6: Continue "<<std::endl;
+
+    return 0;
+}
